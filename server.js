2e276f1fddd9ca706969d0c38fd7cbba4c352552
tubaali13 <tuba.13899@gmail.com>
2022-08-09 22:48:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,119 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const mysql = require('mysql');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'tuba_ali1234',
+	database: 'loginApp',
+	port: 3306,
+	multipleStatements: true
+})
+
+db.connect((err) => {
+	if(err)
+		throw err;
+	else
+		console.log("Connected");
+})
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res) => {
+    res.json("Success");
+    console.log("!!!");
+})
+
+function queryExec(sql,res) {
+	db.query(sql, function (error, results) {
+    if (error) {
+        throw error;
+        res.json("Error");
+    }
+    res.json(results);
+    console.log(results);
+	});
+}
+
+app.post('/login', (req, res) => {
+	var username = req.body.username,
+	password = req.body.password;
+	
+	let sql = `SELECT * FROM users where username = '${username}' 
+	AND password = '${password}';`
+
+	db.query(
+		`SELECT * FROM users where username = '${username}' `,
+
+		(err, result) => {
+    		if (err) {
+        		res.send("Error");
+    		}
+    	if(result.length > 0) {
+    		bcrypt.compare(password, result[0].password, (error, response) => {
+    			if(response) {
+    				res.send(result);
+    			} else {
+    				res.send(error);
+    			}
+    		})
+    	}
+    	else {
+    		res.send({message: "User does'nt exist"});
+    	}
+    });
+})
+
+app.post('/register', (req, res) => {
+	var username = req.body.username,
+	password = req.body.password;
+
+	bcrypt.hash( password, saltRounds, (err, hash) => {
+		if(err) {
+			console.log(err);
+		}
+		db.query (
+			`INSERT INTO users (username, password) VALUES ('${username}','${hash}');`,
+			(err, result) => {
+				if(err) {
+					console.log(err)
+				}
+				else {
+					res.send(result);
+				}
+			}
+		)
+	})
+
+})
+
+app.get('/userProfile/:id', (req, res) => {
+	var userId = req.params.id;
+
+	db.query (
+		`SELECT * from users where id = '${userId}';`,
+		(err, result) => {
+			if(err) {
+				console.log(err)
+			}
+			else {
+				res.send(result);
+			}
+		})
+	console.log("hello");
+})
+
+
+app.listen(3001, err => {
+	console.log("The app is running at port 3001");
+});
